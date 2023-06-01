a5265d1f31411e9a975f1cdfbfef842620acf27f
Liam Robinson <robin502@purdue.edu>
2023-06-01 10:28:43 -0600

    maybe cp7 wheels build now

--- setup.py
+++ setup.py
@@ -27,6 +27,8 @@ setup(
     install_requires=['numpy'],
     package_data={'gravitas': ['libgrav.h']},
     classifiers=[
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
